4d5f219274c771b02e470655bfd27b25ef2055c3
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-06 22:13:38 -0700

    added the connection to mysql

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,25 @@
 // Set up code to connect node to mysql
 // export connection
+var mysql = require("mysql");
 
-// import require connection.js into orm.js
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
+
+// make the connection
+connection.connect(function(err) {
+  if(err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+
+
+// export conection for our ORM to use
+module.exports = connection;
