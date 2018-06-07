d52b461b2c993847c3f146ae9535e00459376a75
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-07 00:13:17 -0700

    adding routes to controllers

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,7 +1,19 @@
-// import express
-// and burger.js
+// require express
+var express = require("express");
+//create router for express
+var router = express.Router();
+
+//import the model burger.js file to use its database functions
+var burger = require("../models/burger.js");
+
 
 
 
 
 // create the router for the app and export the router at the end of your files
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
