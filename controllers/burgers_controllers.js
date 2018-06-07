a5858eb7456c754b1dcba128fd091755e1cbeec7
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-07 00:09:45 -0700

    added folders and psuedocode

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,7 @@
+// import express
+// and burger.js
+
+
+
+
+// create the router for the app and export the router at the end of your files
---
+++ models/burger.js
@@ -0,0 +1,10 @@
+// import the orm.js on to burger.js
+
+// also
+
+//create the code that will call the orm functions using burger specific input for the orm
+
+
+
+
+// export at the end of burger.js
