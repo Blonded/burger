5b9fdd326fd956ad12b3a7047dba827dd66ea709
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-06 22:37:06 -0700

    added connections to server.js

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
 // require the following npm packages
 var express = require("express");
-var bodyparser = require("body-parser");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the public directory in app directory
+// This is mostly used for CSS and HTML styling files
+// for everything included in the folder 'public'.
+app.use(express.static("public"));
+
+app.use(bodyParser.urlendcoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs ({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
+
+app.use("/", routes);
+
+app.listen(port function() {
+  // log server side when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
