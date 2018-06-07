b6a608ae848ab3baf63957918c1f04a8f1d0b0bd
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-07 01:16:38 -0700

    add file into update

--- config/orm.js
+++ config/orm.js
@@ -4,15 +4,83 @@ var connection = require("../config/connection.js");
 // controllers. These are the methods you will need in order to retrieve and store
 // data in your DATABASE
 
+// Export the ORM object in module exports.
+
+// the following function loops through and creates an array of question marks
+// and turns it into a string. this is needed in order to write the query (to pass three values).
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
 // These will be your ORM's -- homemade.
 // selectAll()
 // insertOne()
 // updateOne()
-//
-// Export the ORM object in module exports.
+
+
+// !!! could possibly use function to
+// !!! convert object key/value pairs to sql syntax !!!
+
+var orm = {
+  // to selectALL
+  all: function(tableInput, cb) {
+    var queryString = "INSERT INTO" + tableInput + ";";
+    connection.query(queryString, function(err,result) {
+      if(err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // to CREATE - or Insert.
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO" + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if(err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // to UPDATE:
+  update: function(table, objColVals, conditions, cb){
+    var queryString = "UPDATE" + table;
+
+    queryString += " SET ";
+   queryString += objToSql(objColVals);
+   queryString += " WHERE ";
+   queryString += condition;
+
+   console.log(queryString);
+   connection.query(queryString, function(err, result) {
+     if (err) {
+       throw err;
+     }
+     cb(result);
+  });
+
+}
+
+};
+// ^ closes obj orm.
 
 
 
 
-// module.exports = orm.js (?????)
+// export the orm object for the folder:[model] - burger.js
 module.exports = orm;
