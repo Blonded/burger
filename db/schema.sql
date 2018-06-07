a9ac7f17f036b93428ade3d8193fbc0de44b7030
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-06 20:59:44 -0700

    completed burger sql table

--- db/schema.sql
+++ db/schema.sql
@@ -3,14 +3,15 @@
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 -- use burgers_db
-USE DATABASE burgers_db;
+USE burgers_db;
 
 -- Create a table for burgers_db, include id, burger name, devoured(boolean),
  -- and a primary key.
 CREATE table burgers_db (
 id INT(10) AUTO_INCREMENT NOT NULL,
+burger_id VARCHAR(40) NOT NULL,
 burger_name VARCHAR(40) NOT NULL,
-devoured BOOLEAN NOT NULL default 'TRUE',
+devoured BOOLEAN DEFAULT false,
 
 PRIMARY KEY(id) NOT NULL
-);
+)
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+-- insert data into mysql database
+
+INSERT INTO burgers_db (burger_id, burger_name, devoured)
+VALUES (1, 'Double Double Burger', false);
+VALUES (2, 'Cheese and Bacon Burger', false);
+VALUES (3, 'Vegan Burger', false);
+VALUES (4, 'Fried Egg Burger', false);
