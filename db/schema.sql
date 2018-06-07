33872f51afa9b88d821d92610fa6f1291d78ac17
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-06 21:18:02 -0700

    fixed sql table errors

--- db/schema.sql
+++ db/schema.sql
@@ -7,11 +7,11 @@ USE burgers_db;
 
 -- Create a table for burgers_db, include id, burger name, devoured(boolean),
  -- and a primary key.
-CREATE table burgers_db (
-id INT(10) AUTO_INCREMENT NOT NULL,
-burger_id VARCHAR(40) NOT NULL,
-burger_name VARCHAR(40) NOT NULL,
-devoured BOOLEAN DEFAULT false,
+CREATE TABLE burgers (
+    id INT(10) AUTO_INCREMENT NOT NULL,
+    burger_id VARCHAR(40) NOT NULL,
+    burger_name VARCHAR(40) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
 
-PRIMARY KEY(id) NOT NULL
-)
+    PRIMARY KEY(id)
+  )
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,9 @@
 
 -- insert data into mysql database
 
-INSERT INTO burgers_db (burger_id, burger_name, devoured)
-VALUES (1, 'Double Double Burger', false);
-VALUES (2, 'Cheese and Bacon Burger', false);
-VALUES (3, 'Vegan Burger', false);
-VALUES (4, 'Fried Egg Burger', false);
+INSERT INTO burgers (burger_id, burger_name, devoured)
+VALUES
+    (1, 'Double Double Burger', false),
+    (2, 'Cheese and Bacon Burger', false),
+    (3, 'Vegan Burger', false),
+    (4, 'Fried Egg Burger', false)
