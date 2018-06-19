217093948c7daf83d5c41c818aee509adf4df829
Blonded <Lena@Lenas-MacBook-Pro.local>
2018-06-19 14:19:34 -0700

    update

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,4 +6,4 @@ VALUES
     (1, 'Double Double Burger', false),
     (2, 'Cheese and Bacon Burger', false),
     (3, 'Vegan Burger', false),
-    (4, 'Fried Egg Burger', false)
+    (4, 'Fried Egg Burger', false);
