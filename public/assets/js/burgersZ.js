0f254864f3d7c7e93c3199ee550f16d2f9f5eafe
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-19 20:06:45 -0700

    fixing reference errors

--- public/assets/js/burgersZ.js
+++ public/assets/js/burgersZ.js
@@ -1,17 +1,17 @@
 // this is where the onclick functions will be coded out for the buttons
 
 $(function(){
-  $(".change-burger".on("click", function(event) {
+  $(".change-order".on("click", function(event) {
     var id = $(this).data("id");
     var newBurger = $(this).data("newburger");
 
-    var newBigBurger = {
+    var newOrder = {
       burger: newBurger
     };
 
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
-      data: newBigBurger
+      data: newOrder
     }).then(
       function() {
         console.log("changed burger to ", newBurger);
@@ -24,15 +24,15 @@ $(function(){
 $.ajax(".create-form").on("submit", function(event){
   event.preventDefault();
 
-  var newBurger = {
-    name: $("#burg").val().trim(),
+  var newBurgerChoice = {
+    name: $("#newOrder").val().trim(),
     hungry: $("[name=hungry]:checked").val()trim()
   };
 
 // send post request
 $.ajax("/api/burgers", {
   type: "POST",
-  data: newBurger
+  data: newBurgerChoicegit
 }).then(
   function(){
     console.log("Created new burger");
