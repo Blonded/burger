f9670dc26f96be6bb83d6f5b0de3af561464e12b
Blonded <Lena@lenas-mbp.attlocal.net>
2018-06-21 01:13:46 -0700

    button styling

--- db/schema.sql
+++ db/schema.sql
@@ -13,11 +13,3 @@ CREATE TABLE burgers (
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
   );
-
-
-  INSERT INTO burgers (burger_name, devoured)
-  VALUES
-      ('Double Double Burger', false),
-      ('Cheese and Bacon Burger', false),
-      ('Vegan Burger', false),
-      ('Fried Egg Burger', false);
